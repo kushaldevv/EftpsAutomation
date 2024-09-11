ccda11108041d8b2525dc575962f6bcb8304e487
Kushal Devkota <kushal@terpmail.umd.edu>
2024-09-11 14:07:21 -0400

    handle multiple csv files now

--- main.py
+++ main.py
@@ -1,46 +1,86 @@
+from dataclasses import dataclass
 import json
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from datetime import datetime
+import tkinter as tk
+import os
+
+@dataclass(frozen=True)
+class PaymentInfo:
+    name: str
+    ein: str
+    pin: str
+    internet_password: str
+    year: str
+    quarter: str
+    payment_amount: str
+    social_security_amount: str
+    medicare_amount: str
+    federal_tax_amount: str
+
+# Seperate file with sensitive data
+data = None
+with open('company_data.json', 'r') as file:
+    data = json.load(file)
+
+# Get the list of csv files in the tax_liability folder
+folder_path = 'tax_liability'
+file_list = os.listdir(folder_path)
+
+# Iterate through each file
+for file_name in file_list:
+    file_path = os.path.join(folder_path, file_name)
+    
+    df = pd.read_csv(file_path, header=None)
+    company_name = df.iloc[0][0].split('\n')[0].strip()
+
+
+    company_details = data[company_name]
+    ein = company_details['ein']
+    pin = company_details['pin']
+    internet_password = company_details['internet_password']
+
+    date_range = df.iloc[1][0]
+    start_date = datetime.strptime(date_range.split(' to ')[0], '%m/%d/%Y')
+    year = start_date.year
+    quarter = (start_date.month - 1) // 3 + 1
+
+
+    payment_amount = ((df[df[2] == '941 Total'].values[0][-1]).replace(',', ''))
+    social_security_amount = ((df[df[2] == 'Social Security Wages'].values[0][-1]).replace(',', ''))
+    medicare_amount = (df[df[2] == 'Medicare Wages & Tips'].values[0][-1]).replace(',', '')
+    federal_tax_amount = (df[df[2] == 'Federal Income Tax'].values[0][-1]).replace(',', '')
+
+    if (float(social_security_amount) + float(medicare_amount) + float(federal_tax_amount)) != float(payment_amount):
+        print("Amounts don't match for company: ", company_name)
+        exit()
+    
+    payment_info = PaymentInfo(
+        name=company_name,
+        ein=ein,
+        pin=pin,
+        internet_password=internet_password,
+        year=year,
+        quarter=quarter,
+        payment_amount=payment_amount,
+        social_security_amount=social_security_amount,
+        medicare_amount=medicare_amount,
+        federal_tax_amount=federal_tax_amount,
+    )
+
+    print(payment_info)
+
+window = tk.Tk()
+window.mainloop()
+settle_date = '09/11/2024'
 
 # login_link = 'http://127.0.0.1:5500/'
 # driver = webdriver.Chrome()
 # driver.get(login_link)
-settle_date = '09/11/2024'
 
-# get this from the csv file for each company monthly report
-
-df = pd.read_csv('test.csv', header=None)
-
-company_name = df.iloc[0][0].split('\n')[0].strip()
-
-date_range = df.iloc[1][0]
-start_date = datetime.strptime(date_range.split(' to ')[0], '%m/%d/%Y')
-year = start_date.year
-quarter = (start_date.month - 1) // 3 + 1
-
-
-payment_amount = ((df[df[2] == '941 Total'].values[0][-1]).replace(',', ''))
-social_security_amount = ((df[df[2] == 'Social Security Wages'].values[0][-1]).replace(',', ''))
-medicare_amount = (df[df[2] == 'Medicare Wages & Tips'].values[0][-1]).replace(',', '')
-federal_tax_amount = (df[df[2] == 'Federal Income Tax'].values[0][-1]).replace(',', '')
-
-if (float(social_security_amount) + float(medicare_amount) + float(federal_tax_amount)) != float(payment_amount):
-    print("Amounts don't match")
-    exit()
-
-# move this to top after we implement the for each csv file
-# with open('company_data.json', 'r') as file:
-#     data = json.load(file)
-
-# company_details = data[company_name]
-# ein = company_details['ein']
-# pin = company_details['pin']
-# internet_password = company_details['internet_password']
-
-# print(ein, pin, internet_password)
 # try:
 #     ein1_input = driver.find_element(by=By.NAME, value="EIN1")
 #     ein2_input = driver.find_element(by=By.NAME, value="EIN2")
